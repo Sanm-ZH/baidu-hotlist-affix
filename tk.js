708942bdf5c594efc05bb6d15b8f71d5ddd5891f
Sanm-ZH <zhxpvip7@gmail.com>
2021-04-27 13:44:31 +0800

    Update 支持百度知道热榜列表

--- tk.js
+++ tk.js
@@ -1,41 +1,49 @@
 // ==UserScript==
 // @name        baidu hot list Affix
 // @namespace   https://github.com/Sanm-ZH/baidu-hotlist-affix
-// @match       *://www.baidu.com/s?*
+// @match       https://www.baidu.com/s?*
+// @match       https://zhidao.baidu.com/search?*
 // @grant       none
-// @version     1.0.1
+// @version     1.2.0
 // @author      sanm-zh
 // @icon        https://www.baidu.com/img/baidu_85beaf5496f291521eb75ba38eacbd87.svg
 // @description 百度热榜固定
 // ==/UserScript==
 
 (function () {
-  // 调用dom处理
-  hotListDomHandel();
+    var reg = /(http|https):\/\/(www|zhidao).baidu.com\/(s|search)*/
+    if(reg.test(location.href)) {
+      // 调用dom处理
+      hotListDomHandel();
+    }
 })();
-
+  
 // 获取热榜dom
 function hotListDomHandel() {
-  var hotEle = document.getElementsByClassName('FYB_RD');
-  if (hotEle.length) {
-      var isHotList = hotEle[0].getElementsByClassName('cr-title')[0].title === '百度热榜';
-      if (isHotList) {
-          var top = getelementtopagetop(hotEle[0]),
-              url = location.href;
-          window.onscroll = function () {
-              throttle(changeHotListStyle(hotEle[0], top, url), 1000);
-          }
+    var isZhidao = location.href.indexOf('//zhidao.baidu.com/') > -1;
+    var selector = '';
+    if (isZhidao) {
+      selector = '#right-billboard';
+    } else {
+      selector = '.FYB_RD';
+    }
+    var hotEle = document.querySelectorAll(selector);
+    if (hotEle.length) {
+      var top = getelementtopagetop(hotEle[0]),
+          url = location.href;
+      window.onscroll = function () {
+          throttle(changeHotListStyle(hotEle[0], top, url), 1000);
       }
-  } else {
-      return false;
-  }
+    } else {
+        return false;
+    }
 }
-
+  
 // 固定热点列表样式
 function changeHotListStyle(hotEle, top, url) {
     // url变化重新获取热榜dom
     if(location.href !== url) {
-      hotListDomHandel();
+    hotListDomHandel();
     }
     var scrollTop = document.documentElement.scrollTop;
     if (scrollTop > top - 70) {
@@ -44,16 +52,16 @@ function changeHotListStyle(hotEle, top, url) {
         hotEle.style = '';
     }
 }
-
-// 获取el到顶部的距离
-function getelementtopagetop(el) {
-    if (el.parentElement) {
-        var h = el.parentElement.offsetTop === el.offsetTop ? 0 : el.offsetTop;
-        return getelementtopagetop(el.parentElement) + h;
-    }
-    return el.offsetTop;
-}
-
+  
+  // 获取el到顶部的距离
+  function getelementtopagetop(el) {
+      if (el.parentElement) {
+          var h = el.parentElement.offsetTop === el.offsetTop ? 0 : el.offsetTop;
+          return getelementtopagetop(el.parentElement) + h;
+      }
+      return el.offsetTop;
+  }
+  
 // 节流
 function throttle(fn, delay) {
     var prevTime = Date.now();
